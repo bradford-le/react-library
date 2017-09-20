04d2678a9912cadc7002fbc6672caf8ce59fdcd9
Brad Le <brad.le444@gmail.com>
2017-09-20 16:59:23 -0700

    List Books and move

---
+++ src/components/SearchBooks.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { BookShelf } from './BookShelf';
+
+class SearchBooks extends Component {
+ 
+  render() {
+    
+    const {searchFunc, books} = this.props;
+
+    return (
+      <div className="search-books">
+         <div className="search-books-bar">
+            <Link to="/" className="close-search" >Close</Link>
+            <div className="search-books-input-wrapper">
+                <input type="text" onChange={searchFunc} placeholder="Search by title or author"/>
+            </div>
+          </div>
+          <BookShelf title="" filter="" books={books} onUpdateShelf={this.props.onUpdateShelf} />
+      </div>
+        )
+    }
+}
+
+export default SearchBooks;
